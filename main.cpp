d675ab78b6ad686237dfa6a7fe27b9640c26c143
va3wam <33527895+va3wam@users.noreply.github.com>
2021-08-25 20:00:54 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,57 @@
+/******************************************************************************
+ * @file main.cpp
+ *
+ * @mainpage pioProjectTemplate 
+ * 
+ * @section intro_sec Introduction
+ *
+ * This is an Arduino sketch template. Replace all text in tis heading section
+ * with yur project's information. Note that this template uses an 80 column
+ * layout to support small screen formtas as well as side by side file viewing 
+ * on wide screen formats.
+ *
+ * @section dependencies Dependencies
+ * 
+ * This sketch depends on the libraries located in the main.h file.
+ *
+ * @section author Author(s)
+ *
+ * Written by Old Squire and Doug Elliott.
+ *
+ * @section license license
+ *
+ * Copyright 2021 the Aging Apprentice 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions: The above copyright
+ * notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.  
+ *****************************************************************************/
+#include <main.h> // Header file for all libraries needed by this program.
+
+/**
+ * @brief Standard Arduino initialization routine.
+ * =================================================================================*/
+void setup() 
+{
+  
+} // setup()
+
+/**
+ * @brief Standard Arduino main loop.
+ * =================================================================================*/
+void loop() 
+{
+  
+} // loop()  
