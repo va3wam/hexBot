7fcaa3bda75c5ea39b9a08e3f6a838008e1f8e36
va3wam <va3wam@gmail.com>
2021-08-30 09:19:31 -0400

    Added known network list

--- include/main.h
+++ include/main.h
@@ -1,3 +1,4 @@
 #include <Arduino.h> // Arduino Core for ESP32. Comes with PlatformIO.
 #include <huzzah32_gpio_pins.h> // Map pins on Adafruit Huzzah32 dev board to friendly names.
 #include <hexbot_gpio_pins.h> // Map Hexbot specific pin naming to generic development board pin names. 
+#include <known_networks.h> // String arrays of known Access Points and their passwords.
