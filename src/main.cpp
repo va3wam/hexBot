f6af63a307b69bbd523777da25097faad31b0951
va3wam <va3wam@gmail.com>
2021-09-11 22:08:49 -0400

    Added I2c logic. Removed PWM logic.

--- src/main.cpp
+++ src/main.cpp
@@ -44,9 +44,6 @@
 
 #include <Wire.h> // I2C communication.
 #include <Adafruit_PWMServoDriver.h> // https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library.
-#define I2C_bus0_speed 400000 // Define speed of I2C bus 2. Note 400KHz is the upper speed limit for ESP32 I2C
-#define I2C_bus0_SDA 21 // Define pin on the board used for Serial Data Line (SDA) for I2C bus 2
-#define I2C_bus0_SCL 22 // Define pin on the board used for Serial Clock Line (SCL) for I2C bus 2
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire); // I2C address 0x40 (default) on Wire1.
 #define SERVO_FREQ 50 // Analog servos run at a freq of 50Hz creating a period of (1/50*1000 = 20ms). 
                       // Can be between 40Hz and 1600Hz. Servo motors tyically use 50Hz.
@@ -54,15 +51,15 @@ portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED; // Mutex to prevent ISR and mai
 volatile int interruptCounter = 0; // Number of signals from servo driver not processed.
 volatile long last, now; // Track freq of servo motorcontroller PWM signal.
 const byte interruptPin = 14; // GPIO14 is physical pin 5 on 30 pin Devkit V1 board.
-uint32_t oscFreq; // Oscillator freq of the PCA9865 servo motor driver. Board specific.
+uint32_t oscFreq = 27000000; // Oscillator freq of the PCA9865 servo motor driver. Board specific.
+
 #define PIN_SERVO_FEEDBACK 15 // Connect white PWM pin, 15 = last on last block. Monitor PWM from servo driver.
 int numberOfInterrupts = 0; // Total number of servo driver signals in total.
 unsigned long timer; // Milli count for next action.
 int32_t cadencePeriod; // How long between phases in millis.
-uint8_t leftDriverPinNum = 1; // Target pin on right motor driver.
+uint8_t leftDriverPinNum = 14; // Target pin on left motor driver.
 uint16_t pwmOnCnt = 0; // PWM counter when to raise signal.
 uint16_t pwmOffCnt = 300; // PWM counter when to lower signal.
-
 
 /**
  * @brief ISR for PWM signal from PCA9685.
@@ -81,35 +78,18 @@ void IRAM_ATTR handleInterrupt()
  * ==========================================================================*/
 void setup() 
 {
-   String result[2];
-   result[false] = "false";
-   result[true] = "true"; 
    setupSerial(); // Set serial baud rate. 
    Serial.println("<setup> Start of setup.");  
+   Wire.begin(I2C_bus0_SDA,I2C_bus0_SCL,I2C_bus0_speed);
+   scanBus0();
+//   Wire1.begin(I2C_bus1_SDA,I2C_bus1_SCL,I2C_bus1_speed);
+//   scanBus1();
    network.connect(); // Start WiFi connection. 
    startWebServer(); // Start up web server.
    showCfgDetails(); // Show all configuration details.
-   //connectToMqttBroker(); // Connect to MQTT broker.
    bool tmp = connectToMqttBroker(); // Connect to MQTT broker.
    Serial.print("<setup> Connected to MQTT broker = ");
-   Serial.print(tmp); Serial.print("( "); Serial.print(result[tmp]); Serial.println(")");
-
-   pwm.begin();
-   pwm.setOscillatorFrequency(oscFreq); // Make function to adjust this until the PWM 
-                                        // signal on pin 11 to hit as close to 50Hz as 
-                                        // possible. Using Saleae Logic 8 unit outut 
-                                        // ranges from 49.72Hz to 50.51Hz with most 
-                                        // readings around 50.1Hz. 
-   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates.  
-   pwm.setPWM(PIN_SERVO_FEEDBACK,0,2048); // half of time high, half of time low
-   delay(10);
-   now = millis();
-   pinMode(interruptPin, INPUT);
-   attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, RISING);
-
-   Wire.begin(I2C_bus0_SDA,I2C_bus0_SCL,I2C_bus0_speed);
-   timer = millis(); // Set next phase timer.
-   cadencePeriod = 1000; // Millis between cadence phases.
+   Serial.print(tmp); Serial.print(" ("); Serial.print(result[tmp]); Serial.println(")");
    Serial.println("<setup> End of setup."); 
 } // setup()
 
