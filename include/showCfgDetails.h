49503a7c1c7354da01af3d4c4c3ceaffaa5d98b4
va3wam <va3wam@gmail.com>
2021-09-14 16:21:19 -0400

    Added status flags and conditional logic.

--- include/showCfgDetails.h
+++ include/showCfgDetails.h
@@ -3,9 +3,16 @@
 #define showCfgDetails_h // Precompiler macro used for precompiler check.
 
 #include <main.h> // Header file for all libraries needed by this program.
+#include <connectToMqttBroker.h> // MQTT broker functions.
+//#include <i2c.h> // Scan I2C buses to see what devices are present.
 const char* HOST_NAME_PREFIX = "Hexbot"; // Prefix for our unique network name.
 aaChip appCpu; // Access information about the ESP32 application microprocessor (Core1).
 aaNetwork network(HOST_NAME_PREFIX); // WiFi session management.
+bool networkConnected = false;
+bool mqttBrokerConnected = false;
+bool oledConnected = false;
+bool motorController1Connected = false;
+bool motorController2Connected = false;
 
 /** 
  * @brief Show the environment details of this application.
@@ -15,7 +22,49 @@ void showCfgDetails()
    Serial.println("<showCfgDetails> Robot Configuration Report");
    Serial.println("<showCfgDetails> ==========================");
    appCpu.cfgToConsole(); // Display core0 information on the console.
-   network.cfgToConsole(); // Display network information on the console.
+   if(networkConnected == true)
+   {
+      Serial.println("<showCfgDetails> Network connection status = TRUE");
+      network.cfgToConsole(); // Display network information on the console.
+      if(mqttBrokerConnected == true)
+      {
+         Serial.println("<showCfgDetails> MQTT broker connection status = TRUE");
+         Serial.print("<showCfgDetails> MQTT broker IP address = ");
+         Serial.println(getMqttBrokerIP());
+      } // if
+      else
+      {
+         Serial.println("<showCfgDetails> MQTT broker connection status = FALSE");
+      } // else
+   } // if
+   else
+   {
+      Serial.println("<showCfgDetails> Network connection status = FALSE");
+   } // else
+   if(oledConnected == true)
+   {
+      Serial.println("<showCfgDetails> OLED connection status = TRUE.");
+   } // if
+   else
+   {
+      Serial.println("<showCfgDetails> OLED connection status = FALSE.");
+   } // else
+   if(motorController1Connected == true)
+   {
+      Serial.println("<showCfgDetails> Left servo driver connection status = TRUE.");
+   } // if
+   else
+   {
+      Serial.println("<showCfgDetails> Left servo driver connection status = FALSE.");
+   } // else
+   if(motorController2Connected == true)
+   {
+      Serial.println("<showCfgDetails> Right servo driver connection status = TRUE.");
+   } // if
+   else
+   {
+      Serial.println("<showCfgDetails> Right servo driver connection status = FALSE.");
+   } // else
 } //showCfgDetails()
 
 #endif // End of precompiler protected code block
