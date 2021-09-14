9d9c9b69eb08c102d87a8ed839fe1097bfe2de16
va3wam <va3wam@gmail.com>
2021-09-14 16:19:23 -0400

    Added library and function references.

--- include/main.h
+++ include/main.h
@@ -14,23 +14,25 @@
 #include <aaWebService.h> // Realtime web-based network config and OTA code updates.
 #include <aaFlash.h> // Use Flash memory to store values that persist past reboot.
 #include <aaMqtt.h> // Use MQTT for remote management and monitoring.
-#include <huzzah32_gpio_pins.h> // Map pins on Adafruit Huzzah32 dev board to friendly names.
-#include <hexbot_gpio_pins.h> // Map Hexbot specific pin naming to generic development board pin names. 
 #include <known_networks.h> // String arrays of known Access Points and their passwords.
 #include <InverseK.h> // https://github.com/cgxeiji/CGx-InverseK
 #include <Wire.h> // Required for I2C communication
+#include <Adafruit_PWMServoDriver.h> // https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library.
 /*******************************************************************************
  * @section codeModules Functions put into files according to function.
  * @details Order functions here in a way that ensures that variables get 
  * declared in one function prior to being referenced by another function. 
  *******************************************************************************/
+#include <huzzah32_gpio_pins.h> // Map pins on Adafruit Huzzah32 dev board to friendly names.
+#include <hexbot_gpio_pins.h> // Map Hexbot specific pin naming to generic development board pin names. 
 #include <setupSerial.h> // Serial port initialization.
 #include <showCfgDetails.h> // Show the environment details of this application.
 #include <startWebServer.h> // Start up the web server service. 
 #include <connectToMqttBroker.h> // Establish connect to the the MQTT broker.
 #include <monitorWebServer.h> // Monitor the web server service.
 #include <i2c.h> // Scan I2C buses to see what devices are present.
-
+#include <servo.h> // Control servo motors.
+#include <oled.h> // Control OLED.
 /************************************************************************************
  * @section mainDeclare Declare functions.
  ************************************************************************************/
@@ -39,6 +41,13 @@ void showCfgDetails(); // Show the environment details of this application.
 void startWebServer(); // Start up the local web server service.
 void monitorWebServer(); // Look after pending web server requests.
 bool connectToMqttBroker(); // Establish connect to the the MQTT broker. 
+void identifyDevice(int deviceAddress);
+void scanBus0(); // ID devices connected to I2C bus0.
+void scanBus1(); //  ID devices connected to I2C bus1.
+void initServo(); // Initialize serv motor control.
+void updatePhase();  // Basic test routing for servo motors.
+void initOled(); // Set up OLED.
+void checkOledButtons(); // Check oled buttons. 
 void setup(); // Arduino mandatory function #1. Runs once at boot. 
 void loop(); // Arduino mandatory function #2. Runs continually.
 
