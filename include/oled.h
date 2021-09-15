0e70b98983cb97a3f40dc52fa5d4b920aae38493
va3wam <va3wam@gmail.com>
2021-09-15 14:55:47 -0400

    Added centering routines.

--- include/oled.h
+++ include/oled.h
@@ -8,9 +8,14 @@
 #include <hexbot_gpio_pins.h> // GPIO pin uses
 
 Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
-bool buttonA_flag = false;
-bool buttonB_flag = false;
-bool buttonC_flag = false;
+bool buttonA_flag = false; // Flag used by hardware ISR for button A.
+bool buttonB_flag = false; // Flag used by hardware ISR for button B.
+bool buttonC_flag = false; // Flag used by hardware ISR for button C.
+
+uint8_t oledX = 128; // Screen width in pixels.
+uint8_t oledY = 64; // Screen height in pixels.
+uint8_t textBaseX = 6; // Smallest font width in pixels.
+uint8_t textBaseY = 8; // Smallest font height in pixels.
 
 /**
  * @brief Hardware Interrupt Service Routine for Button A on OLED display.
@@ -37,22 +42,51 @@ void IRAM_ATTR ButtonC_ISR()
 } // ButtonC_ISR()
 
 /**
+ * @brief Places a text message centrered vertically and horizontally.
+ * 
+ * @param msg A text message to be displayed.
+ * @param fontSize Multiplier of base text size (6px X 8px). Usually 1-3.
+ * @param fontColour SH110X_WHITE is the usual choice here.
+ * ==========================================================================*/
+void placeTextVHcentre(String msg, uint8_t fontSize, uint16_t fontColour) 
+{
+   display.setTextSize(fontSize);
+   display.setTextColor(fontColour);
+   uint8_t x = (oledX - (textBaseX * fontSize * msg.length())) / 2; 
+   uint8_t y = (oledY - (textBaseY * fontSize)) / 2;  
+   display.setCursor(x, y);
+   display.print(msg);
+} // placeTextVHcentre
+
+/**
+ * @brief Places a text message centrered horizontally.
+ * 
+ * @param msg A text message to be displayed.
+ * @param fontSize Multiplier of base text size (6px X 8px). Usually 1-3.
+ * @param fontColour SH110X_WHITE is the usual choice here.
+ * ==========================================================================*/
+void placeTextHcentre(String msg, uint8_t fontSize, uint16_t fontColour) 
+{
+   display.setTextSize(fontSize);
+   display.setTextColor(fontColour);
+   uint8_t x = (oledX - (textBaseX * fontSize * msg.length())) / 2; 
+   display.setCursor(x, display.getCursorY());
+   display.println(msg);
+} // placeTextHcentre()
+
+/**
  * @brief Display the splash screen.
  * ==========================================================================*/
 void displaySplashScreen() 
 {
-   // Clear the buffer.
-   display.clearDisplay();
-   display.display();
-   display.setRotation(1);
-   // Display splash
-   display.setTextSize(3);
-   display.setTextColor(SH110X_WHITE);
-   display.setCursor(10,20);
-   display.print("HEXBOT");
-   delay(10);
-   yield();      
-   display.display(); // actually display all of the above
+   
+   display.clearDisplay(); // Clear the buffer.
+   display.display(); // Display cleared buffer.
+   display.setRotation(1); // Not sure what this does but its in the example.
+   placeTextVHcentre("HEXBOT", 3, SH110X_WHITE); // Place logo on screen.
+   delay(10); // Wait for buffer.
+   yield(); // Not sure what this does but was in example.     
+   display.display(); // Actually display all of the above
 } // displaySplashScreen()
 
 /**
@@ -64,11 +98,9 @@ void checkOledButtons()
    {
       buttonA_flag = false; 
       display.clearDisplay();
-      display.setTextSize(1);
-      display.setTextColor(SH110X_WHITE);
-      display.setCursor(25,0);
-      display.println("Configuration \n");
-      display.print("Robot: ");
+      display.setCursor(0, 0);
+      placeTextHcentre("Configuration", 1, SH110X_WHITE);
+      display.print("\nRobot: ");
       display.println(WiFi.localIP());
       display.print("Broker: ");
       display.println(getMqttBrokerIP());
@@ -80,10 +112,9 @@ void checkOledButtons()
    {
       buttonB_flag = false; 
       display.clearDisplay();
-      display.setTextSize(1);
-      display.setTextColor(SH110X_WHITE);
-      display.setCursor(32,0);
-      display.println("Blank Menu \n");
+      display.setCursor(0, 0);
+      placeTextHcentre("Blank Menu", 1, SH110X_WHITE);
+      display.print("\nShow options here.");
       delay(10);
       yield();      
       display.display(); // actually display all of the above   
@@ -100,27 +131,15 @@ void checkOledButtons()
  * ==========================================================================*/
 void initOled() 
 {
-//   Serial.begin(115200);
-
    Serial.println("<initOled> 128x64 OLED FeatherWing test");
    display.begin(0x3C, true); // Address 0x3C default
    Serial.println("<initOled> OLED begun");
-
-   // Clear the buffer.
-//   display.clearDisplay();
-//   display.display();
-
-//   display.setRotation(1);
-//   Serial.println("<initOled> Button test");
-
-   pinMode(BUTTON_A, INPUT_PULLUP);
-   pinMode(BUTTON_B, INPUT_PULLUP);
-   pinMode(BUTTON_C, INPUT_PULLUP);
-
-   attachInterrupt(BUTTON_A, ButtonA_ISR, RISING);
-   attachInterrupt(BUTTON_B, ButtonB_ISR, RISING);
-   attachInterrupt(BUTTON_C, ButtonC_ISR, RISING);
-
+   pinMode(BUTTON_A, INPUT_PULLUP); // Make button A pin input with weak pullup.
+   pinMode(BUTTON_B, INPUT_PULLUP); // Make button B pin input with weak pullup.
+   pinMode(BUTTON_C, INPUT_PULLUP); // Make button C pin input with weak pullup.
+   attachInterrupt(BUTTON_A, ButtonA_ISR, RISING); // Assign ISR for button A.
+   attachInterrupt(BUTTON_B, ButtonB_ISR, RISING); // Assign ISR for button B.
+   attachInterrupt(BUTTON_C, ButtonC_ISR, RISING); // Assign ISR for button C.
    displaySplashScreen();
 } // setup()
 
