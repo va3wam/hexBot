8a9d79e99e9f5a4a513820f007434093e3e75deb
va3wam <va3wam@gmail.com>
2021-10-25 22:50:52 -0400

    Fixed name of precompiler #ifndef variable.

--- include/known_networks.h
+++ include/known_networks.h
@@ -1,9 +1,9 @@
 /*************************************************************************************************************************************
  TODO Encrypt this file.
  *************************************************************************************************************************************/ 
-#ifndef amKnownNetworks_h
+#ifndef knownNetworks_h // Start of precompiler check to avoid dupicate inclusion of this code block.
 
-#define amKnownNetworks_h
+#define knownNetworks_h // Precompiler macro used for precompiler check.
 
 static const int numKnownAPs = 5; // Number of known APs that the Robot knows how to connect to
 const String SSID[numKnownAPs] = { "MN_LIVINGROOM", "MN_WORKSHOP_2.4GHz", "MN_DS_OFFICE_2.4GHz", "MN_OUTSIDE", "borfpiggle"};
