34bfe00031e382bb2ca1680f377a46fad9c0ff59
va3wam <va3wam@gmail.com>
2021-10-27 18:49:23 -0400

    Added global vards and function definitions

--- include/main.h
+++ include/main.h
@@ -25,7 +25,20 @@
 
 /*******************************************************************************
  * @section mainVars Global variable definitions.
+ * @details In order to get rid of "error: 'xxx' was not declared in this scope"
+ * compiler errors list a function from any of the include files here. 
  *******************************************************************************/
+// Config related variables
+const char* HOST_NAME_PREFIX = "Hexbot"; // Prefix for our unique network name.
+aaChip appCpu; // Access information about the ESP32 application microprocessor (Core1).
+aaNetwork network(HOST_NAME_PREFIX); // WiFi session management.
+bool networkConnected = false;
+bool mqttBrokerConnected = false;
+bool oledConnected = false;
+bool motorController1Connected = false;
+bool motorController2Connected = false;
+bool mobilityStatus = false;
+int8_t displayPage = 1;
 // MQTT related variables.
 aaFlash flash; // Non-volatile memory management. 
 aaMqtt mqtt; // Publish and subscribe to MQTT broker. 
@@ -52,22 +65,30 @@ const uint8_t PWM_RESOLUTION = 8; // ESP32 can go up to 8 bit PWM resolution.
 #define PWM_RED_CHANNEL 0 // ESP32 has 16 channels which can generate 16 independent waveforms. Use 0 for RGB red.
 #define PWM_GREEN_CHANNEL 1 // ESP32 has 16 channels which can generate 16 independent waveforms. Use 1 for RGB green.
 #define PWM_BLUE_CHANNEL 2 // ESP32 has 16 channels which can generate 16 independent waveforms. Use 2 for RGB blue.
+// DAE Inverted kinematic related variables.
+const uint8_t footLen = 11; // Distance from ankle to toe (foot) is 11cm. 
+const uint8_t shinLen = 7.5; // Distance from knee to ankle (shin) is 7.5cm.
+const uint8_t toeOffset = 17; // Angle that toe is offset from 90deg of ankle joint is 17 degrees.
+const uint8_t origXOffset = 2.92; // Distance the knee is offset from the origin along the x axis.
 
 /************************************************************************************
- * @section funDeclare Declare functions found in the include files.
+ * @section funcDeclare Declare functions found in the include files.
  * @details In order to get rid of "error: 'xxx' was not declared in this scope"
- * compiler errors list a function from any of the include filles here. 
+ * compiler errors list a function from any of the include files here. 
  ************************************************************************************/
 void rotateDisplay(int8_t); 
 void displaySplashScreen(String);
-
+// Config ralted functions
+void showCfgDetails();
+void displayCfgDetails(int8_t);
+void checkBoot();
 // MQTT related functions
-bool connectToMqttBroker(aaNetwork&);
+bool connectToMqttBroker(aaNetwork &);
 boolean isValidNumber(String);
 uint32_t convertStrToUint32_t(String);
 uint8_t convertStrToUint8_t(String);
 int8_t convertStrToInt8_t(String);
-bool checkNumArg(int8_t, String*);
+bool checkNumArg(int8_t, String *);
 bool processCmd(String);
 IPAddress getMqttBrokerIP();
 void checkMqtt();
@@ -78,6 +99,10 @@ void loadRgbColour();
 void setCustRgbColour(uint32_t, uint32_t, uint32_t);
 void setStdRgbColour(uint8_t);
 void setupStatusLed();
+// DAE Inverted kinematic related functions.
+int32_t mapDegToPWM(float *, float *);
+void anglesToCoords(float, float, float, float *, float *, float *);
+void coordsToAngles(float, float, float, float *, float *, float *);  
 
 /*******************************************************************************
  * @section codeModules Functions put into files according to function.
@@ -86,17 +111,17 @@ void setupStatusLed();
  *******************************************************************************/
 #include <huzzah32_gpio_pins.h> // Map pins on Adafruit Huzzah32 dev board to friendly names.
 #include <hexbot_gpio_pins.h> // Map Hexbot specific pin naming to generic development board pin names. 
-#include <terminal.h> // Serial port management.
-#include <configDetails.h> // Show the environment details of this application.
-#include <web.h> // Manage locally hosted web service. 
+#include <terminal.cpp> // Serial port management.
+#include <configDetails.cpp> // Show the environment details of this application.
+#include <web.cpp> // Manage locally hosted web service. 
 #include <rgbLed.cpp> // Control status LEDs.
-#include <network.h> // Control networking activities.
-#include <mqttBroker.h> // Establish connect to the the MQTT broker.
-#include <i2c.h> // Scan I2C buses to see what devices are present.
-#include <ikLibrary.h> // InverseK.h inverted kinematic functions.
-#include <servoLegs.h> // Control leg servos.
-#include <oled.h> // Control OLED.
-#include <daeIK.h> // Doug's inverted kinetics functions. 
+#include <network.cpp> // Control networking activities.
+#include <mqttBroker.cpp> // Establish connect to the the MQTT broker.
+#include <i2c.cpp> // Scan I2C buses to see what devices are present.
+#include <ikLibrary.cpp> // InverseK.h inverted kinematic functions.
+#include <servoLegs.cpp> // Control leg servos.
+#include <oled.cpp> // Control OLED.
+#include <daeIK.cpp> // Doug's inverted kinetics functions. 
 
 /************************************************************************************
  * @section mainDeclare Declare functions in main.cpp.
