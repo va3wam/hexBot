5f7344ffdb17c08b77cc34f728ef95418e968a04
va3wam <va3wam@gmail.com>
2021-10-27 20:47:04 -0400

    Changed huzzah GPIO pin names.

--- include/hexbot_gpio_pins.h
+++ include/hexbot_gpio_pins.h
@@ -1,21 +1,22 @@
-/*************************************************************************************************************************************
- Note that the physical pin count starts at the reset button on long pin side of the Huzzah32 development board.
- *************************************************************************************************************************************/
+/******************************************************************************
+ Note that the physical pin count starts at the reset button on long pin side 
+ of the Huzzah32 development board.
+ ******************************************************************************/
 #ifndef _HEXBOT_GPIO_PINS_H // Start of conditional preprocessor code that only allows this library to be included once
 
 #define _HEXBOT_GPIO_PINS_H // Preprocessor variable used by above check
 
 #include <huzzah32_gpio_pins.h> // Mapping of ESP32 pins to Huzzah32 development board pins
 
-const int8_t G_I2C_BUS0_SDA = PIN_LBL_SDA; // Serial Data Line (SDA) for I2C bus 0, physical pin 17
-const int8_t G_I2C_BUS0_SCL = PIN_LBL_SCL; // Serial Clock Line (SCL) for I2C bus 0, physical pin 18
-const int8_t G_I2C_BUS1_SDA = PIN_LBL_TX; // Serial Data Line (SDA) for I2C bus 1, physical pin 15
-const int8_t G_I2C_BUS1_SCL = PIN_LBL_21; // Serial Clock Line (SCL) for I2C bus 1, physical pin 16
-const int8_t G_BUTTON_A = PIN_LBL_15; // Button A on OLED featherboard, physical pin 21
-const int8_t G_BUTTON_B = PIN_LBL_32; // Button B on OLED featherboard, physical pin 20
-const int8_t G_BUTTON_C = PIN_LBL_14; // Button C on OLED featherboard, physical pin 19
-const int8_t G_RESET_RED_LED = PIN_LBL_27; // Red LED in reset button, physical pin 23
-const int8_t G_RESET_BLUE_LED = PIN_LBL_33; // Blue LED in reset button, physical pin 22
-const int8_t G_RESET_GREEN_LED = PIN_LBL_MO; // Green LED in reset button, physical pin 12
+const int8_t G_I2C_BUS0_SDA = PIN_17_LBL_SDA; // Serial Data Line (SDA) for I2C bus 0, physical pin 17
+const int8_t G_I2C_BUS0_SCL = PIN_18_LBL_SCL; // Serial Clock Line (SCL) for I2C bus 0, physical pin 18
+const int8_t G_I2C_BUS1_SDA = PIN_15_LBL_TX; // Serial Data Line (SDA) for I2C bus 1, physical pin 15
+const int8_t G_I2C_BUS1_SCL = PIN_16_LBL_21; // Serial Clock Line (SCL) for I2C bus 1, physical pin 16
+const int8_t G_BUTTON_A = PIN_21_LBL_15; // Button A on OLED featherboard, physical pin 21
+const int8_t G_BUTTON_B = PIN_20_LBL_32; // Button B on OLED featherboard, physical pin 20
+const int8_t G_BUTTON_C = PIN_19_LBL_14; // Button C on OLED featherboard, physical pin 19
+const int8_t G_RESET_RED_LED = PIN_23_LBL_27; // Red LED in reset button, physical pin 23
+const int8_t G_RESET_BLUE_LED = PIN_22_LBL_33; // Blue LED in reset button, physical pin 22
+const int8_t G_RESET_GREEN_LED = PIN_12_LBL_MO; // Green LED in reset button, physical pin 12
 
 #endif // End of conditional preprocessor code
--- include/huzzah32_gpio_pins.h
+++ include/huzzah32_gpio_pins.h
@@ -1,4 +1,4 @@
-/******************************************************************************
+/*******************************************************************************
  
  # Table of Adafruit Huzzah32 development board pin assignments.
  
@@ -35,7 +35,7 @@
  |: 14  :|: I/O :|: Analog input A6 on ADC #2 and also GPIO #14. | 
  
  Note that you can only read analog inputs on ADC #1 once WiFi has started.
- ******************************************************************************/
+ *******************************************************************************/
 #ifndef _HUZZAH32_GPIO_PINS_H // Start of conditional preprocessor code that only allows this library to be included once
 
 #define _HUZZAH32_GPIO_PINS_H // Preprocessor variable used by above check
@@ -50,7 +50,8 @@
  * and the 12 pin header on the right. Start counting physical pins from the 
  * upper left corner. This is pin 1.
  * 
- * The variable names roughly follow the silk screen labelling on the bard. 
+ * The variable names work as follows: 
+ * PIN_<physical pin number>_<silk screen label>.
  * The variable values are the GPIO numbers used in code to reference pins.
  * Physical pins 1 through 4 are not used by our code, nor are physical pins
  * 26 through 28 (see pin comments below for details).
@@ -63,30 +64,30 @@
 // Physical pin 2 is labeled 3V
 // Physical pin 3 is labeled NC
 // Physical pin 4 is labeled GND
-const int8_t PIN_LBL_A0 = 26; // Physical pin 5
-const int8_t PIN_LBL_A1 = 25; // Physical pin 6
-const int8_t PIN_LBL_A2 = 34; // Physical pin 7
-const int8_t PIN_LBL_A3 = 39; // Physical pin 8
-const int8_t PIN_LBL_A4 = 36; // Physical pin 9
-const int8_t PIN_LBL_A5 = 4; // Physical pin 10
-const int8_t PIN_LBL_SCK = 5; // Physical pin 11
-const int8_t PIN_LBL_MO = 18; // Physical pin 12
-const int8_t PIN_LBL_MI = 19; // Physical pin 13
-const int8_t PIN_LBL_RX = 16; // Physical pin 14
-const int8_t PIN_LBL_TX = 17; // Physical pin 15
-const int8_t PIN_LBL_21 = 21; // Physical pin 16
+const int8_t PIN_5_LBL_A0 = 26; // Physical pin 5. Input and output.
+const int8_t PIN_6_LBL_A1 = 25; // Physical pin 6. Input and output.
+const int8_t PIN_7_LBL_A2 = 34; // Physical pin 7. Input only.
+const int8_t PIN_8_LBL_A3 = 39; // Physical pin 8. Input only.
+const int8_t PIN_9_LBL_A4 = 36; // Physical pin 9. Input only.
+const int8_t PIN_10_LBL_A5 = 4; // Physical pin 10. Input and output.
+const int8_t PIN_11_LBL_SCK = 5; // Physical pin 11. Output only.
+const int8_t PIN_12_LBL_MO = 18; // Physical pin 12. Output only.
+const int8_t PIN_13_LBL_MI = 19; // Physical pin 13. Input only.
+const int8_t PIN_14_LBL_RX = 16; // Physical pin 14. Input only.
+const int8_t PIN_15_LBL_TX = 17; // Physical pin 15. Output only.
+const int8_t PIN_16_LBL_21 = 21; // Physical pin 16. Input and output.
 ///////////////////////////////////////////////////////////////////////////////
 // Variables used to reference the pins in the right 12 pin socket.
 ///////////////////////////////////////////////////////////////////////////////
-const int8_t PIN_LBL_SDA = 23; // Physical pin 17 
-const int8_t PIN_LBL_SCL = 22; // Physical pin 18
-const int8_t PIN_LBL_14 = 14; // Physical pin 19
-const int8_t PIN_LBL_32 = 32; // Physical pin 20
-const int8_t PIN_LBL_15 = 15; // Physical pin 21
-const int8_t PIN_LBL_33 = 33; // Physical pin 22
-const int8_t PIN_LBL_27 = 27; // Physical pin 23
-const int8_t PIN_LBL_12 = 12; // Physical pin 24
-const int8_t PIN_LBL_13 = 13; // Physical pin 25
+const int8_t PIN_17_LBL_SDA = 23; // Physical pin 17. Input and output. 
+const int8_t PIN_18_LBL_SCL = 22; // Physical pin 18. Input and output.
+const int8_t PIN_19_LBL_14 = 14; // Physical pin 19. Input and output.
+const int8_t PIN_20_LBL_32 = 32; // Physical pin 20. Input and output.
+const int8_t PIN_21_LBL_15 = 15; // Physical pin 21. Input and output.
+const int8_t PIN_22_LBL_33 = 33; // Physical pin 22. Input and output.
+const int8_t PIN_23_LBL_27 = 27; // Physical pin 23. Input and output.
+const int8_t PIN_24_LBL_12 = 12; // Physical pin 24. Output only.
+const int8_t PIN_25_LBL_13 = 13; // Physical pin 25. Input and output.
 // Physical pin 26 is labeled USB
 // Physical pin 27 is labeled EN
 // Physical pin 28 is labeled BAT
